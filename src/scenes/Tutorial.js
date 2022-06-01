c8fa4b7963579d243846e177ef7bd3ace1462ffc
Anika <anmahaja@ucsc.edu>
2022-06-01 05:11:03 -0700

    added tutorial

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -5,11 +5,19 @@ class Tutorial extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('tutorial', './assets/tutorial.png');
+        this.load.image('rightArrow', './assets/rightArrow.png');
     }
 
     create() {
+        // add background
+        this.background = this.add.tileSprite(0, 0, 1280, 960, 'tutorial').setOrigin(0, 0);
 
+        this.arrow = this.add.sprite(1100, 20, 'rightArrow').setScale(0.5).setOrigin(0, 0).setInteractive();
+
+        this.arrow.on('pointerdown', ()=> {
+            this.scene.start('Menu');
+        })
     }
 
 
